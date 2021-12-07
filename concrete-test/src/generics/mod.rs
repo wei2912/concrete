a9797ef6109385d5869384cdaeb7eb48ea9a8256
Alexandre Péré <alexandre.pere@zama.ai>
2021-12-07 16:07:29 +0100

    feat(concrete_test): add lwe_ciphertext_cleartext_mul test

--- concrete-test/src/generics/mod.rs
+++ concrete-test/src/generics/mod.rs
@@ -3,5 +3,6 @@
 //! Every submodule here is expected to contain a generic `test` function which can be instantiated
 //! with different engine types to verify the correctness of an operation.
 
+pub mod lwe_ciphertext_cleartext_discarding_multiplication;
 pub mod lwe_ciphertext_discarding_keyswitch;
 pub mod lwe_ciphertext_encryption;
