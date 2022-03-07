32d629778573f72c20e1cc3010c17914416c4c98
Ben <ben.curtis@zama.ai>
2022-03-07 14:58:42 +0000

    update backend implementation

--- concrete-core/src/backends/core/implementation/engines/glwe_ciphertext_tensor_product.rs
+++ concrete-core/src/backends/core/implementation/engines/glwe_ciphertext_tensor_product.rs
@@ -2,14 +2,14 @@ use concrete_commons::parameters::GlweSize;
 
 use crate::backends::core::engines::CoreEngine;
 use crate::backends::core::entities::{FourierGlweCiphertext64, GlweCiphertext32, GlweCiphertext64, FourierGlweCiphertext32};
-use crate::backends::core::private::crypto::glwe::{FourierGlweCiphertext, GlweCiphertext};
 use crate::backends::core::private::crypto::glwe::GlweCiphertext as ImplGlweCiphertext;
+use crate::backends::core::private::crypto::glwe::FourierGlweCiphertext as ImplFourierGlweCiphertext;
 use crate::prelude::{AbstractEngine, GlweCiphertextTensorProductEngine, GlweCiphertextTensorProductError};
 use crate::specification::entities::GlweCiphertextEntity;
 
 /// # Description:
 /// Implementation of [`GlweTensorProductEngine`] for [`CoreEngine`] that operates on 32-bit
-/// integers.
+/// integer Glwe Ciphertexts.
 impl GlweCiphertextTensorProductEngine<GlweCiphertext32, GlweCiphertext32, GlweCiphertext32>
     for CoreEngine{
 
@@ -23,11 +23,8 @@ impl GlweCiphertextTensorProductEngine<GlweCiphertext32, GlweCiphertext32, GlweC
 
 
 
-
-
     unsafe fn tensor_product_glwe_ciphertext_unchecked(&mut self,
-                                                       // we need to use this on FourierGLWECiphertexts32
-                                                       input1: &FourierGlweCiphertext32,
+                                                       input1: &GlweCiphertext32,
                                                        input2: &GlweCiphertext32) -> GlweCiphertext32 {
 
         let mut ciphertext = ImplGlweCiphertext::allocate(
@@ -36,11 +33,36 @@ impl GlweCiphertextTensorProductEngine<GlweCiphertext32, GlweCiphertext32, GlweC
             GlweSize(input1.glwe_dimension().0 * (3 + input1.glwe_dimension().0) * (1/2)),
         );
 
-        // .0 accesses GLWE ciphertext inside input1
-        input1.0.tensor_product(input2)
+        let fft_buffer1 = &mut buffers.fft_buffers.first_buffer;
+        let fft_buffer2 = &mut buffers.fft_buffers.second_buffer;
+
+        // convert the two GLWE ciphertexts of interest to the fourier domain
+        let mut fourier_1 = ImplFourierGlweCiphertext::allocate(
+            Complex64::Zero,
+            input1.polynomial_size(),
+            GlweSize(input1.glwe_dimension().0),
+        );
+
+        let mut fourier_2 = ImplFourierGlweCiphertext::allocate(
+            Complex64::Zero,
+            input1.polynomial_size(),
+            GlweSize(input1.glwe_dimension().0),
+        );
+
+        fourier_1.fill_with_forward_fourier(self, fft_buffer1);
+        fourier_2.fill_with_forward_fourier(input1, fft_buffer2);
+
+        // perform the tensor product (in the fourier domain)
+        fourier_1.0.tensor_product(fourier_2);
+
+        // convert the result back to the coefficient domain
+        ciphertext.convert_glwe_ciphertext(fourier_1.0);
     }
 }
 
+/// # Description:
+/// Implementation of [`GlweTensorProductEngine`] for [`CoreEngine`] that operates on 64-bit
+/// integer Glwe Ciphertexts.
 impl GlweCiphertextTensorProductEngine<GlweCiphertext64, GlweCiphertext64, GlweCiphertext64>
 for CoreEngine{
 
@@ -53,12 +75,8 @@ for CoreEngine{
     }
 
 
-
-
-
     unsafe fn tensor_product_glwe_ciphertext_unchecked(&mut self,
-                                                       // we need to use this on FourierGLWECiphertexts32
-                                                       input1: &FourierGlweCiphertext64,
+                                                       input1: &GlweCiphertext64,
                                                        input2: &GlweCiphertext64) -> GlweCiphertext64 {
 
         let mut ciphertext = ImplGlweCiphertext::allocate(
@@ -67,9 +85,33 @@ for CoreEngine{
             GlweSize(input1.glwe_dimension().0 * (3 + input1.glwe_dimension().0) * (1/2)),
         );
 
-        // .0 accesses GLWE ciphertext inside input1
-        input1.0.tensor_product(input2)
+        let fft_buffer1 = &mut buffers.fft_buffers.first_buffer;
+        let fft_buffer2 = &mut buffers.fft_buffers.second_buffer;
+
+        // convert the two GLWE ciphertexts of interest to the fourier domain
+        let mut fourier_1 = ImplFourierGlweCiphertext::allocate(
+            Complex64::Zero,
+            input1.polynomial_size(),
+            GlweSize(input1.glwe_dimension().0),
+        );
+
+        let mut fourier_2 = ImplFourierGlweCiphertext::allocate(
+            Complex64::Zero,
+            input1.polynomial_size(),
+            GlweSize(input1.glwe_dimension().0),
+        );
+
+        fourier_1.fill_with_forward_fourier(self, fft_buffer1);
+        fourier_2.fill_with_forward_fourier(input1, fft_buffer2);
+
+        // perform the tensor product (in the fourier domain)
+        fourier_1.0.tensor_product(fourier_2);
+
+        // convert the result back to the coefficient domain
+        ciphertext.convert_glwe_ciphertext(fourier_1.0);
     }
 }
+
+
 
 
